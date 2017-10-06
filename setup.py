a771156fe629d04341afa723248d6f5e23c370ac
Bart Frenk <bart.frenk@gmail.com>
2017-10-06 10:19:05 +0200

    Add setup.py for uploading to PyPi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='constraints',
+    packages=['constraints'],
+    version='0.1',
+    description='Generate validators from SQLAlchemy models',
+    author='Bart Frenk',
+    author_email='bart.frenk@gmail.com',
+    url='https://github.com/bartfrenk/constraints',
+    download_url='https://github.com/bartfrenk/constraints/archive/0.1.tar.gz',
+    keywords=['validation', 'sqlalchemy'],
+    classifiers=[],
+)
