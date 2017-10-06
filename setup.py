a12209c9fa82289fd7edaf04f9b72de21de2f55a
Bart Frenk <bart.frenk@gmail.com>
2017-10-06 10:34:46 +0200

    Update version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='constraints',
     packages=['constraints'],
-    version='0.1',
+    version='0.2.2',
     description='Generate validators from SQLAlchemy models',
     author='Bart Frenk',
     author_email='bart.frenk@gmail.com',
     url='https://github.com/bartfrenk/constraints',
-    download_url='https://github.com/bartfrenk/constraints/archive/0.1.tar.gz',
+    download_url='https://github.com/bartfrenk/constraints/archive/0.2.2.tar.gz',
     keywords=['validation', 'sqlalchemy'],
     classifiers=[],
 )
